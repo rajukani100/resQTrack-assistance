d8fd69ef3bc98128c21175b94e257d161e484b7e
Shyam <pshyam550@gmail.com>
2026-02-13 12:00:00 +0530

    added accident alert screen to fronted

---
+++ resQTrack - for bystanders/frontend/src/pages/Accident-alert.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import AccidentCard from "../components/AccidentCard";
+
+export default function AccidentAlert() {
+ 
+
+  return (
+    <>
+    <AccidentCard/>
+    </>
+  );
+}
