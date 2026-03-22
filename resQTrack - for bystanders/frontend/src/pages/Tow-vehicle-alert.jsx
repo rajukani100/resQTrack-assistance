8c2842a2d52f73077fbc1c9bbe83fde2eb606a56
Shyam <pshyam550@gmail.com>
2026-03-22 17:00:47 +0530

    bystander add tow vehicle alert

---
+++ resQTrack - for bystanders/frontend/src/pages/Tow-vehicle-alert.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import TowVehicleCard from "../components/TowVehicleCard";
+
+export default function TowVehicleAlert() {
+ 
+
+  return (
+    <>
+    <TowVehicleCard/>
+    </>
+  );
+}
